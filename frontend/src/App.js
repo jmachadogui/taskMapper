d56d6e3da95c31b1ebbbd816349803b1eabbee26
machado <guilhermemachado>
2020-05-16 16:39:22 -0300

    pom configured. Initial screen design created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        Bem Vindo
+        <div className="box-container">
+          <div className="box">
+            <span className="box-title">Entrar</span>
+          </div>
+          <div className="box">
+            <span className="box-title">Cadastrar</span>
+          </div>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
